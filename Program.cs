8c677ac0c4171bd9f0ce2f164833d53c85832c6e
LovroMacicK <lovro.macic@student.um.si>
2025-11-16 12:04:32 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipelines.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
