80eda8b95a92a7dadcf35aa8f8dbdda43a982d7e
LovroMacicK <lovro.macic@student.um.si>
2025-11-17 14:21:50 +0100

    Setup

--- City-Feedback.Tests/obj/Debug/net8.0/City-Feedback.Tests.AssemblyInfo.cs
+++ City-Feedback.Tests/obj/Debug/net8.0/City-Feedback.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("City-Feedback.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec495272eda55ebdde30eff62d388f5e60ff2f6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39a87fd50c3a221fd331d931d760fe47c1a1b69")]
 [assembly: System.Reflection.AssemblyProductAttribute("City-Feedback.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("City-Feedback.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
